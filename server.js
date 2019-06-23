7c94947f4288ebf7e616f0ab9f849be5e8b05f4b
pizzadeesnutz <arcadio@thearcadio.com>
2019-06-23 10:50:04 -0400

    Testing
    Test deployment to Heroku

---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+var dbUrl = 'mongodb+srv://user:user@learning-node-rretj.mongodb.net/test?retryWrites=true&w=majority'
+
+var Message = mongoose.model('Message', {
+  name: String,
+  message: String
+})
+
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages) => {
+    res.send(messages)
+  })
+})
+
+app.post('/messages', (req, res) => {
+  var message = new Message(req.body)
+
+  message.save((err) => {
+    if(err) sentStatus(500)
+
+    io.emit('message', req.body)
+    res.sendStatus(200)
+  })
+})
+
+io.on('connection', (socket) => {
+  console.log('user connected')
+})
+
+mongoose.connect(dbUrl, {useMongoClient: true}, (err) => {
+  console.log('mongo db connection', err)
+})
+
+var server = http.listen(3000, () => {
+  console.log('server is listening on port: ', server.address().port)
+})
---
+++ spec/server.spec.js
@@ -0,0 +1,16 @@
+var request = require('request')
+
+describe('get messages', () => {
+  it('should return 200 Ok', (done) => {
+    request.get('http://localhost:3000/messages', (err, res) =>{
+      expect(res.statusCode).toEqual(200)
+      done()
+    })
+  })
+  it('should return a list that is not empty', (done) => {
+    request.get('http://localhost:3000/messages', (err, res) =>{
+      expect(JSON.parse(res.body).length).toBeGreaterThan(0)
+      done()
+    })
+  })
+})
