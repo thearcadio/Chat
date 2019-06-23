05292f28109b3a5ed0f8ffbc1bb2c56e633afd8b
pizzadeesnutz <arcadio@thearcadio.com>
2019-06-23 13:05:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ mongoose.connect(dbUrl, {useMongoClient: true}, (err) => {
   console.log('mongo db connection', err)
 })
 
-var server = http.listen(3000, () => {
+var server = http.listen(process.env.PORT || 3000, () => {
   console.log('server is listening on port: ', server.address().port)
 })
